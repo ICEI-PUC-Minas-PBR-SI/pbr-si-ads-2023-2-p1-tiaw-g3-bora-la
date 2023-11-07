d18b30c747f50e595b8f0128a5bf937c8bd0cfbf
João Gabreil <skullkidmajoramawk@gmail.com>
2023-11-07 16:59:26 -0300

    tela de usuario e estabelecimento atualizados

--- src/usuario.js
+++ src/usuario.js
@@ -1,16 +1,26 @@
-/* em andamento
-const exampleModal = document.getElementById('modal')
-if (exampleModal) {
-  exampleModal.addEventListener('show.bs.modal', event => {
- 
-    const button = event.relatedTarget
- 
-    const recipient = button.getAttribute('data-bs-whatever')
-  
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+/*
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
-  })
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }*/
